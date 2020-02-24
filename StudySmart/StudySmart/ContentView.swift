cc22259cb7e9ca2690d18106909299952f9fb835
dlim303 <deli4631@colorado.edu>
2020-02-23 19:06:02 -0700

    start of the project

---
+++ StudySmart/StudySmart/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  StudySmart
+//
+//  Created by Dennis Lim on 2/23/20.
+//  Copyright © 2020 Dennis Lim. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
